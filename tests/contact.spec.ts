f22f2d3f4ff732dbc53f80846c1290eed7074e74
Andrey-Ivantsov <andrey-ivantsov@mail.ru>
2023-05-22 15:09:48 +0400

    add faker for contact page test

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import ContactPage from '../pages/contact.page';
+import { faker } from '@faker-js/faker'
 
 test.describe('first task', () => {
 
@@ -11,7 +12,7 @@ test.describe('first task', () => {
   })
 
   test('go to conact page and fill the form', async ({ page }) => {
-    await contactPage.submitForm('andrey', 'test@mail.test', '1234567890')
+    await contactPage.submitForm(faker.person.fullName(), faker.internet.email(), faker.phone.number())
     await expect.soft(contactPage.succesTxt).toHaveText('Thanks for contacting us! We will be in touch with you shortly')
   })
 })
